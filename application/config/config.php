cb22b4c639998de4fcc1f2b3a10cd650bfa3d967
HafidhAlbarM <43909248+HafidhAlbarM@users.noreply.github.com>
2020-12-08 19:55:51 +0700

    add session setting

--- application/config/config.php
+++ application/config/config.php
@@ -27,7 +27,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = 'https://alsanmotor.herokuapp.com/';
 
 //tambahan config session
-$config['sess_save_path'] = sys_get_temp_dir();
+$config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 1440;
+$config['sess_save_path'] = FCPATH . 'application/cache/sessions/';
+$config['sess_match_ip'] = TRUE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
