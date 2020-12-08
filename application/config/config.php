c1135d70f6b98a293b05c13d8d6eb7bc4fddc5db
HafidhAlbarM <43909248+HafidhAlbarM@users.noreply.github.com>
2020-12-08 20:04:59 +0700

    set sess_save_path

--- application/config/config.php
+++ application/config/config.php
@@ -26,14 +26,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $config['base_url'] = '';
 $config['base_url'] = 'https://alsanmotor.herokuapp.com/';
 
-//tambahan config session
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 1440;
-$config['sess_save_path'] = FCPATH . 'application/cache/sessions/';
-$config['sess_match_ip'] = TRUE;
-$config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
@@ -390,7 +382,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
