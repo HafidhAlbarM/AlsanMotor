bff22583358d1abe26c7b26db3b1088180805cae
HafidhAlbarM <43909248+HafidhAlbarM@users.noreply.github.com>
2020-12-08 20:16:24 +0700

    add base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = '';
-$config['base_url'] = 'https://alsanmotor.herokuapp.com/';
+$config['base_url'] = 'https://alsan.herokuapp.com/';
 // $config['base_url'] = 'http://localhost:8080/alsan/';
 
 
